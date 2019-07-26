fb88c2bfdba99e048054c90b877628db1ec8e74c
Adam Hoffman <wintros@yahoo.com>
2019-07-26 12:00:58 -0600

    CharacterandLocationCards

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import {Card, Icon, Image} from 'semantic-ui-react'
 
-export default function CharacterCard ({ /* add props */ }) {
-  return (<span>todo: location</span>
-}
+export default function CharacterCard (props) {
+  return (
+    <Card>
+    <Image src={props.image} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>{props.species}<br/> {props.status}</Card.Meta>
+      <Card.Description>
+        Location: {props.location} <br/> Origin: {props.origin}
+      </Card.Description>
+    </Card.Content>    
+  </Card>
+  )}
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Card, Icon, Image } from 'semantic-ui-react'
 
-export default function LocationCard ({ name, type, dimension, residents }) {
+export default function LocationCard (props,{ name, type, dimension, residents }) {
   // image={image}
-  return (<span>todo: location</span>
+  return (
+    <Card>    
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>{props.type}</Card.Meta>
+      <Card.Description>
+        Dimension: {props.dimension}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content>
+      <p>
+        Residents:  {props.residents}
+        
+      </p>
+    </Card.Content>
+  </Card>
   )
 }
